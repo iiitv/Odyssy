d78047291397aae03d2bb2ec5b63b29a60a6cb10
yashLadha <yashladhapankajladha123@gmail.com>
2017-05-22 17:07:22 +0530

    chore: updated query Query updated to select announcements that are greater than current date or their final date is still greater than or equal to current date

--- wsgi/odyssy/basic/views.py
+++ wsgi/odyssy/basic/views.py
@@ -1,9 +1,14 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from announcement.models import Announcement
+import datetime
 
 def index(request):
-    latest_announcement = Announcement.objects.order_by('initDate')[:5]
+    latest_announcement = Announcement.objects.filter(
+        Q(initDate__gte=datetime.date.today())|
+        Q(finDate__gte=datetime.date.today())
+    ).order_by('initDate')[:5]
     context = {
         'latest_announcement': latest_announcement,
     }
