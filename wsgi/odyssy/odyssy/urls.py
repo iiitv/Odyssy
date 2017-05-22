14bb71f89f4f8c9b23c1a3227b83d879c4ee49d6
yashLadha <yashladhapankajladha123@gmail.com>
2017-05-22 14:34:38 +0530

    chore: Changes in urls.py

--- wsgi/odyssy/odyssy/urls.py
+++ wsgi/odyssy/odyssy/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/?', include(admin.site.urls)),
     url(r'^', include('basic.urls')),
-    url(r'announcement/', include('announcement.urls'))
+    url(r'^announcement/', include('announcement.urls'))
 ]
