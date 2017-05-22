232ef4f3ac5a79f87293b71a23209f1e2b30428c
= <201551009@iiitvadodara.ac.in>
2017-05-22 07:03:42 +0530

    - Event description now links to detail of that event. - Number of events on home page can be controlled by env variable

--- wsgi/odyssy/basic/views.py
+++ wsgi/odyssy/basic/views.py
@@ -1,7 +1,9 @@
+import os
+
 from django.shortcuts import render
 
 from events.models import Event
 
 def index(request):
-    events_list = Event.objects.order_by('-event_date')[:3]
+    events_list = Event.objects.order_by('-event_date')[:int(os.environ.get('NUM_EVENTS_ON_INDEX_PAGE', 3 ))]
     return render(request, 'basic/index.html', context={'events': events_list})
--- wsgi/odyssy/events/views.py
+++ wsgi/odyssy/events/views.py
@@ -10,7 +10,7 @@ from .models import Event
 
 def event(request):
     events_list = Event.objects.order_by('-event_date')
-    paginator = Paginator(events_list, os.environ.get('NUM_EVENTS_ON_INDEX_PAGE', 3 ))
+    paginator = Paginator(events_list, 3)
     page = request.GET.get('page')
     try:
         events = paginator.page(page)
@@ -19,11 +19,10 @@ def event(request):
     except EmptyPage:
         events = paginator.page(paginator.num_pages)
     context = {'events': events}
-    return render(request, 'events/index.html', context=context)
+    return render(request, 'events/event_list.html', context=context)
 
 
 def event_detail(request, event_id):
     single_event = get_object_or_404(Event, pk=event_id)
     context = {'event': single_event}
-
-    return render(request, 'events/single_item.html', context=context)
+    return render(request, 'events/event_single_item.html', context=context)
