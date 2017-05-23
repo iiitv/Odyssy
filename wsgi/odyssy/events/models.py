4d7d26cb41c1b11940a8946baa368b3814b83754
= <201551009@iiitvadodara.ac.in>
2017-05-23 10:56:22 +0530

    - Added name field in Events - Changed description field type from CharField to TextField

--- wsgi/odyssy/events/models.py
+++ wsgi/odyssy/events/models.py
@@ -5,12 +5,13 @@ from django.db import models
 
 
 class Event(models.Model):
+    name = models.CharField(max_length=30, default="Sample Name")
     start_time = models.DateTimeField(default=datetime.datetime.now())
     end_time = models.DateTimeField(default=datetime.datetime.now())
-    description = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
     place = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.description + " at " + self.place + " on " + \
+        return self.name + " at " + self.place + " on " + \
                str(self.start_time.day) + " - " + calendar.month_abbr[self.start_time.month] + \
                " - " + str(self.start_time.year)
