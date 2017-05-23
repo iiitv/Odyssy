e4816e5dd76cebe82f5b5aa67692188d7405da3e
Monal5031 <201652013@iiitvadodara.ac.in>
2017-05-23 13:19:32 -0700

    pyling flag disabled

--- wsgi/odyssy/news/urls.py
+++ wsgi/odyssy/news/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
+urlpatterns = [  # pylint: disable=invalid-name
     url(r'^$', views.open_all_news, name='news-all'),
     url(r'^(?P<news_id>[0-9]+)/$', views.open_single_news, name='news-single')
 ]
