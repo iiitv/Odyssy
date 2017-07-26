eb917ae2a3adc9475a977b3c0203f822e3300117
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-07-27 00:48:24 +0530

    Added Student's Corner

--- wsgi/odyssy/more/urls.py
+++ wsgi/odyssy/more/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+url(r'student_corner.html$', views.IndexView.as_view(), name="student_corner"),
+	url(r'^student_corner/(?P<pk>\d+)/$', views.DetailView.as_view(), name="detail"),
     url(r'.html$', views.view_more, name='more-html')
+
 ]
