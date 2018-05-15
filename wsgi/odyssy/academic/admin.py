134647665d856558aa8b53da760961a54d3a7c90
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-05-16 02:01:08 +0530

    Add programme model

--- wsgi/odyssy/academic/admin.py
+++ wsgi/odyssy/academic/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Semester, Course, CourseInSem
+from .models import Programme, Semester, Course, CourseInSem
 
+admin.site.register(Programme)
 admin.site.register(Semester)
 admin.site.register(Course)
 admin.site.register(CourseInSem)
