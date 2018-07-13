9a397e4427ce4b6e13eff9b2f79e55f315864237
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-13 12:57:55 +0530

    remove dev

--- wsgi/odyssy/basic/static/basic/js/secret.js
+++
@@ -1,7 +0,0 @@
-// Do not modify this file
-function openDevPage() {
-    console.log("Mohit Kumar Yadav");
-    var win = window.open("https://github.com/mohitkyadav", '_blank');
-    win.focus();
-}
-// Do not modify this file
